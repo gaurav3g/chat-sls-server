28cc913823457f70cc68b518a5cb4a06453d2d8c
3g <gaurav.g@mindfiresolutions.com>
2020-05-20 01:26:11 +0530

    initial commit

---
+++ client/chat/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from uniauth.decorators import login_required
+import jwt
+import os
+
+
+@login_required
+def index(request):
+    token = jwt.encode({"username": request.user.username}, "FAKE_SECRET",
+            algorithm="HS256").decode("utf-8")
+    return render(request, "chat/index.html",
+            {"endpoint": "wss://mdhu4u131c.execute-api.ap-south-1.amazonaws.com/dev", "token": token})
